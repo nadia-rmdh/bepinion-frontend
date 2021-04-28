58b02a312f369c10b9aaab748d01f50e67b18d76
fabiands97 <fabiands97@gmail.com>
2021-04-28 16:25:39 +0700

    dummy data project

---
+++ src/views/Menu/Project/dummy.js
@@ -0,0 +1,50 @@
+export const data = [
+    {
+        id: 1,
+        name: 'Alexander Monaco',
+        location: 'Jalan Sudirman',
+        createdAt: '2021-04-20',
+        title: 'Perbaikan Jalan',
+        img: require('../../../assets/img/consulting/Legal.png'),
+        likes: 20,
+        desc : 'ini deskripsi tapi juga bukan jadi ini sebenarnya hanya begini begini aja, pokoknya panjang amat gitu deh lho deh ya tau lah yaa gaes',
+        solution: 7,
+        contributors: 3
+    },
+    {
+        id: 2,
+        name: 'Badarawuhi',
+        location: 'Jalan Kaliurang',
+        createdAt: '2021-04-15',
+        title: 'Perbaikan Desa Penari',
+        img: require('../../../assets/img/consulting/HR.png'),
+        likes: 11,
+        desc : 'Diperlukan KKN di Despa Penari buat tumbal',
+        solution: 4,
+        contributors: 4
+    },
+    {
+        id: 3,
+        name: 'Mba Andini',
+        location: 'Jalan Palagan',
+        createdAt: '2021-04-23',
+        title: 'Tambang Emas untuk Saham',
+        img: require('../../../assets/img/consulting/Sales.png'),
+        likes: 11,
+        desc : 'akdgbasiasfabfa asgiafas[fqiuwif asfhasbjdasdn;as;das agahsbdapsjfabsfbasas asfaisbfasjd;askda',
+        solution: 10,
+        contributors: 7
+    },
+    {
+        id: 4,
+        name: 'Dewa AC',
+        location: 'Jalan in Aja Dulu',
+        createdAt: '2021-04-27',
+        title: 'Rekonstruksi Pos Ronda',
+        img: require('../../../assets/img/consulting/Marketing.png'),
+        likes: 100,
+        desc : 'Diperlukan KKN di Despa Penari buat tumbal Diperlukan KKN di Despa Penari buat tumbal Diperlukan KKN di Despa Penari buat tumbal Diperlukan KKN di Despa Penari buat tumbal Diperlukan KKN di Despa Penari buat tumbal Diperlukan KKN di Despa Penari buat tumbal Diperlukan KKN di Despa Penari buat tumbal',
+        solution: 15,
+        contributors: 12
+    },
+]
