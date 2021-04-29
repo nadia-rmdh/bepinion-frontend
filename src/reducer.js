6cc7f3313f1f203eb28b378d5b914cc78307291f
fabiands97 <fabiands97@gmail.com>
2021-04-29 13:16:35 +0700

    fix payload data

--- src/reducer.js
+++ src/reducer.js
@@ -15,19 +15,20 @@ export default (
 ) => {
   switch (action.type) {
     case API_SUCCESS:
-      localStorage.setItem("session", action.payload.session_id);
+      console.log(action)
+      localStorage.setItem("session", action.payload.data.token);
       localStorage.setItem("menu", "3");
       return {
         ...state,
-        user: action.payload,
-        token: action.payload.session_id,
+        user: action.payload.data,
+        token: action.payload.data.token,
         error: null,
         isLoading:false
       };
     case API_ERROR:
       return { ...state, error: "Login Salah", isLoading:false };
     case SET_LOADER:
-      return { ...state, isLoading: action.payload };
+      return { ...state, isLoading: action.payload.data };
     case LOGOUT:
       localStorage.removeItem("menu");
       return { ...state, user: null, error: null, token: null, menu: "3", isAdminPanel: false };
@@ -35,7 +36,7 @@ export default (
       localStorage.setItem("menu", action.value);
       return { ...state, menu: action.value, isAdminPanel: action.value === '2' };
     case SET_USER:
-      return { ...state, user: action.payload };
+      return { ...state, user: action.payload.data };
     default:
       return state;
   }
