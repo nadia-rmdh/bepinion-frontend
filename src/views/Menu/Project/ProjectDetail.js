ca28c6208a7823985e1fc4145f6a888ca8a00a49
fabiands97 <fabiands97@gmail.com>
2021-06-02 13:14:28 +0700

    fix image layout & progress comment

--- src/views/Menu/Project/ProjectDetail.js
+++ src/views/Menu/Project/ProjectDetail.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState, useMemo } from 'react'
-import { Card, CardBody, CardHeader, Carousel, CarouselControl, CarouselIndicators, CarouselItem, Col, Row, Spinner, Button } from 'reactstrap'
+import { Card, CardBody, CardHeader, Carousel, CarouselControl, CarouselIndicators, CarouselItem, Col, Row, Spinner, Button, Input, Form } from 'reactstrap'
 import * as moment from 'moment'
 import ReactMarkdown from "react-markdown";
 import request from '../../../utils/request';
 import { useAuthUser } from '../../../store';
 import { Link, useRouteMatch } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 function ProjectDetail() {
     const matchRoute = useRouteMatch();
@@ -17,6 +18,9 @@ function ProjectDetail() {
     const [activeIndex, setActiveIndex] = useState(0);
     const [animating, setAnimating] = useState(false);
     const [data, setData] = useState([]);
+    const [value, setValue] = useState("")
+    const [submitting, setSubmitting] = useState(false)
+    const notNull = value.replace(/\s/g, "")
 
     useEffect(() => {
         request.get('v1/projects/' + matchRoute.params.code).then(res => {
@@ -64,6 +68,19 @@ function ProjectDetail() {
         }
     }
 
+    const doComment = () => {
+        setSubmitting(true)
+        request.post(`v1/projects/${matchRoute.params.code}/comment`, {comment: value})
+            .then(() => {
+                window.location.reload()
+            })
+            .catch(() => {
+                toast.error('Gagal menambahkan komentar')
+                return;
+            })
+            .finally(() => setSubmitting(false))
+    }
+
     const actionUp = useMemo(() => data?.votes?.find(item => item.userId === user.id && item.type === 'up'), [data, user])
     const actionDown = useMemo(() => data?.votes?.find(item => item.userId === user.id && item.type === 'down'), [data, user])
 
@@ -79,7 +96,7 @@ function ProjectDetail() {
         }
     }, [actionUp, actionDown])
 
-    console.log(data)
+    // console.log(data)
 
     if (loading) {
         return (
@@ -140,6 +157,7 @@ function ProjectDetail() {
                                 onExiting={() => setAnimating(true)}
                                 onExited={() => setAnimating(false)}
                                 key={idx}
+                                className="py-auto"
                             >
                                 <img src={item.storage} alt={'media ' + (idx + 1)} width="100%" />
                             </CarouselItem>
@@ -153,7 +171,7 @@ function ProjectDetail() {
                         }
                     </Carousel>
                 </div>
-                <div className="button-card-project mb-5">
+                <div className="button-card-project mt-3 mb-5">
                     <i className={`fa fa-lg fa-arrow-up mx-1 ${like ? `text-primary scale-click` : ``}`} onClick={() => doLike(data.code)} />
                     <span className="mx-1">{up}</span>
                     <i className={`fa fa-lg fa-arrow-down mx-3 ${unlike ? `text-primary scale-click` : ``}`} onClick={() => doUnLike(data.code)} />
@@ -161,6 +179,32 @@ function ProjectDetail() {
                     <Link to={`/project/${data.code}`}>
                         <Button color="primary" size="sm" className="float-right">Selesaikan Masalah</Button>
                     </Link>
+                    {data?.comment?.length > 1 && <div></div>}
+                    <Row className="mt-4">
+                        <Col xs="2" className="text-center">
+                            <div className={`ml-auto pt-1 round-100 bg-info border-0 text-center align-items-center`}>
+                                <strong>{user?.detail?.fullName?.split('')[0].toUpperCase()}</strong>
+                            </div>
+                        </Col>
+                        <Col xs="10" className="text-right">
+                            <Input
+                                name="comment"
+                                id="comment"
+                                type="textarea"
+                                style={{ borderRadius: "15px" }}
+                                placeholder="Tuliskan deskripsi idemu..."
+                                rows={3}
+                                value={value}
+                                onChange={(e) => setValue(e.target.value)}
+                            />
+                            <Button
+                                style={{borderRadius:'10px'}}
+                                disabled={!notNull}
+                                className="mt-2 btn btn-sm btn-netis-primary px-3 ml-auto"
+                                onClick={doComment}
+                            >{submitting ? <Spinner size="sm" className="my-auto" /> : "Submit"}</Button>
+                        </Col>
+                    </Row>
                 </div>
             </CardBody>
         </Card>
