f0f4f307ab1558d89d5794ed72b464f85d5e4263
fabiands97 <fabiands97@gmail.com>
2021-06-16 12:28:58 +0700

    location state

--- src/views/Menu/Project/Sprint/DesignSprint.js
+++ src/views/Menu/Project/Sprint/DesignSprint.js
@@ -1,24 +1,27 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 import { Table } from 'reactstrap'
 import SprintCard from './SprintCard';
 
 function DesignSprint(){
+    const location = useLocation()
+    // console.log(location)
 
     return(
-        <div style={{minWidth:'90vw'}}>
+        <div style={{minWidth:'95vw', borderRadius:'20px'}}>
             <Table borderless responsive className="table-sprint">
                 <thead>
                     <tr>
-                        <th colSpan={3}>
-                            Nama Tim / Proyek
+                        <th colSpan={3} className="pl-3">
+                            <h5 className="ml-3"><b>Tim {location?.state?.team}</b></h5>
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td><SprintCard title="Analisis ide" column={2} item={10} /></td>
-                        <td><SprintCard title="Prototyping" column={3} item={15} /></td>
-                        <td><SprintCard title="Hasil" column={1} item={1} /></td>
+                        <td className="px-4"><SprintCard title="Analisis ide" column={2} item={10} /></td>
+                        <td className="px-4"><SprintCard title="Prototyping" column={3} item={15} /></td>
+                        <td className="px-4"><SprintCard title="Hasil" column={1} item={1} /></td>
                     </tr>
                 </tbody>
             </Table>
