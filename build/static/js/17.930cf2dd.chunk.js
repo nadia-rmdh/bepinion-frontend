346b1e21bf031f2fc7028d18a239eedfa3b2d4f0
arvinfairuzh <arvin.fairuz.af@gmail.com>
2021-08-31 22:15:38 +0700

    build

---
+++ build/static/js/17.930cf2dd.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppplatform=this.webpackJsonppplatform||[]).push([[17],{499:function(p,o){}}]);
+//# sourceMappingURL=17.930cf2dd.chunk.js.map
---
+++ build/static/js/19.a36984e4.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppplatform=this.webpackJsonppplatform||[]).push([[19],{489:function(p,o){}}]);
+//# sourceMappingURL=19.a36984e4.chunk.js.map
